5160ac1c513262baf6e98ef66c374d78a45ca66c
benedikt <benedikt.bauer96@gmx.de>
2018-08-15 14:02:48 +0200

    test

--- src/main/java/ui/App.java
+++ src/main/java/ui/App.java
@@ -15,7 +15,7 @@ public class App extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hier gehts los");
+        primaryStage.setTitle("Test ob git funktioniert");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
